3e25f3a21b856a6e52b64d538ac4e3b340d08b48
matsuda tomoki <t.m.0011223344@gmail.com>
2016-10-23 14:13:59 +0900

    Add_create_action

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,16 @@
 class ChatsController < ApplicationController
 
   def index
+    @posts = Post.new
+  end
+
+  def create
+   Post.create(post_params)
+   redirect_to :root
   end
   
+  private
+  def post_params
+    params.require(:post).permit(:message,:text_image).merge(group_id: 1,user_id: 1)
+  end
 end
