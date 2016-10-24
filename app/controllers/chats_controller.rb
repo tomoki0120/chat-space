0fc95327d8813f34517eddd41854066a5991daac
matsuda tomoki <t.m.0011223344@gmail.com>
2016-10-24 14:46:19 +0900

    Add_flash_message_on_controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,18 +1,23 @@
 class ChatsController < ApplicationController
 
   def index
-    @posts = Post.new
+    @post = Post.new
+    @posts = Post.order('created_at DESC')
     @user = User.find(1)
     @group = Group.find(2)
   end
 
   def create
-   Post.create(post_params)
-   redirect_to :root
+    post = Post.new(post_params)
+    if post.save
+      redirect_to :root,success: 'Upadate Complete'
+    else
+      redirect_to :root,warning: 'Not complete'
+    end
   end
-  
+
   private
   def post_params
-    params.require(:post).permit(:message,:text_image).merge(group_id: 1,user_id: 1)
+    params.require(:post).permit(:message,:text_image).merge(group_id: 2,user_id: 1)
   end
 end
