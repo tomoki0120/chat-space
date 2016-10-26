a07d3efef73c20238ee43dbb7b471655af6239e6
matsuda tomoki <t.m.0011223344@gmail.com>
2016-10-26 12:23:04 +0900

    Add_validate_for_text_image

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :group
   validates :message,presence: true
+  validates :text_image,presence: true
   mount_uploader :text_image, TextImageUploader
 end
