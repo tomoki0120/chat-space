e8d45f987e725eeccc785c38c9c91f4c89bbb86e
matsuda tomoki <t.m.0011223344@gmail.com>
2016-10-26 12:24:30 +0900

    Ceate_post.rb

---
+++ spec/factories/posts.rb
@@ -0,0 +1,14 @@
+include ActionDispatch::TestProcess
+FactoryGirl.define do
+
+    factory :post do
+      extend ActionDispatch::TestProcess
+      id               1
+      message         'テスト'
+      user_id          1
+      group_id         2
+      created_at      '2016-10-24 13:36:59'
+      updated_at      '2016-10-24 13:36:59'
+      text_image { fixture_file_upload("spec/fixtures/img/sample.jpeg", "image/jpeg") }
+    end
+end
