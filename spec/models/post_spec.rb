2e9283063dcd795351e7b48eef52528353a9eb50
matsuda tomoki <t.m.0011223344@gmail.com>
2016-10-27 11:44:35 +0900

    Fixed_post_spec.rb_to_judge_meessage_to_valid_or_invalid

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 describe Post do
   describe '#create' do
 
+    it "is vaild to save if you have all column" do
+      post  = build(:post)
+      post.valid?
+      expect(post.errors).to include()
+    end
+
     it "is invalid without a message" do
       post = build(:post,message: nil)
       post.valid?
       expect(post.errors[:message]).to include("を入力してください")
     end
 
-    it "is invalid without a text_image" do
-      post = build(:post,text_image: nil)
+    it "is nothing to do with a text_image" do
+      post = build(:post,text_image:nil)
       post.valid?
-      expect(post.errors[:text_image]).to include("を入力してください")
-    end
-
-    it "is invalid" do
-      post  = build(:post)
-      post.valid?
-      expect(post).to be_valid
+      expect(post.errors[:text_image]).to include()
     end
 
   end
