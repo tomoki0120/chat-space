f3014cfaf189a39e8145e96e8ce1c08e0202d381
matsuda tomoki <t.m.0011223344@gmail.com>
2016-10-27 15:29:34 +0900

    Delete_id_and_Introduce_faker(User)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,10 @@ FactoryGirl.define do
 
 
     factory :user do
-      id              '1'
+      nickname {Faker::Name.name }
+      email {Faker::Internet.email}
+      password '12345678'
+      password_confirmation '12345678'
     end
 
 end
