8baf8d7b0d0143f7088b9bad7904809022fa3d7d
matsuda tomoki <t.m.0011223344@gmail.com>
2016-10-27 15:29:55 +0900

    Create_user_and_group

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,24 +2,19 @@ require 'rails_helper'
 
 describe Post do
   describe '#create' do
-
     it "is vaild to save if you have all column" do
-      post  = build(:post)
-      post.valid?
-      expect(post.errors).to include()
+      user = create(:user)
+      group = create(:group,user_id: user.id)
+      post  = create(:post,:message,:text_image,user: user,group: group)
+      expect(post).to be_valid
     end
 
     it "is invalid without a message" do
-      post = build(:post,message: nil)
+      user = create(:user)
+      group = create(:group,user_id: user.id)
+      post = build(:post,:text_image,user: user,group: group)
       post.valid?
       expect(post.errors[:message]).to include("を入力してください")
     end
-
-    it "is nothing to do with a text_image" do
-      post = build(:post,text_image:nil)
-      post.valid?
-      expect(post.errors[:text_image]).to include()
-    end
-
   end
 end
