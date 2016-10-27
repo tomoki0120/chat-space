bfee4d18dc149c01d4eed3ce9a3b9683347c6621
matsuda tomoki <t.m.0011223344@gmail.com>
2016-10-27 11:42:34 +0900

    Delete_validate

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :group
   validates :message,presence: true
-  validates :text_image,presence: true
   mount_uploader :text_image, TextImageUploader
 end
