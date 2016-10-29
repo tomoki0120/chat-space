c72cacf85636ac10bbf08a2ea82e75593ae5514e
matsuda tomoki <t.m.0011223344@gmail.com>
2016-10-29 21:36:56 +0900

    Fixed_created_at_and_updated_at

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
 
     factory :group do
       group_name {Faker::Name.name}
-      created_at {Faker::Date.between(2.days.ago, Date.today)}
-      updated_at {Faker::Date.between(2.days.ago, Date.today)}
+      created_at {Faker::Time.between(2.days.ago, Time.now, :all)}
+      updated_at {Faker::Time.between(2.days.ago, Time.now, :all)}
     end
 
 end
