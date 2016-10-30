7742bb90703ef06380f521cc7d681b765e531dad
matsuda tomoki <t.m.0011223344@gmail.com>
2016-10-31 04:13:41 +0900

    Fixed_gather_create_uset_and_group_by_let!

--- spec/controllers/chats_controller_spec.rb
+++ spec/controllers/chats_controller_spec.rb
@@ -3,31 +3,26 @@ require 'rails_helper'
 describe ChatsController do
   describe 'GET #index' do
 
+      let!(:test_user){create(:user,id:1)}
+      let!(:test_group){create(:group,id:2)}
+
     it "renders the :index template" do
-      test_user = create(:user,id:1)
-      test_group = create(:group,id:2)
       get :index
       expect(response).to render_template :index
     end
 
     it "populates an array of @posts ordered by created_at DESC" do
-      test_user = create(:user)
-      test_group = create(:group,id:2)
       posts = create_list(:post,3,user: test_user,group:test_group)
       get :index
       expect(assigns(:posts)).to match(posts.sort{|a, b| b.created_at <=> a.created_at })
     end
 
     it "assigns the requested contact to @user" do
-      test_group = create(:group,id:2)
-      test_user = create(:user,id:1)
       get :index
       expect(assigns(:user)).to eq test_user
     end
 
     it "assigns the requested contact to @group" do
-      test_group = create(:group,id:2)
-      test_user = create(:user,id:1)
       get :index
       expect(assigns(:group)).to eq test_group
     end
@@ -35,6 +30,9 @@ describe ChatsController do
 
 
   describe 'POST #create' do
+
+      let!(:test_user){create(:user,id:1)}
+      let!(:test_group){create(:group,id:2)}
 
     it "redirect to :index(sucess)" do
       post :create, post: attributes_for(:post)
@@ -47,14 +45,10 @@ describe ChatsController do
     end
 
     it "saves the new contact in the database" do
-      test_group = create(:group,id:2)
-      test_user = create(:user,id:1)
       expect {post :create, post: attributes_for(:post,group: test_group,user: test_user)}.to change(Post, :count).by(1)
     end
 
     it "failed the new contact in the database" do
-      test_group = create(:group,id:2)
-      test_user = create(:user,id:1)
       expect {post :create, post: attributes_for(:post,message: nil,group: test_group,user: test_user)}.not_to change(Post, :count)
     end
   end
