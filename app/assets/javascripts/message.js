b9b035ce5494f79df416ca7c13e909530e92aab7
matsuda tomoki <t.m.0011223344@gmail.com>
2016-11-13 02:02:33 +0800

    Creete_message.js_for_Asynchronous communication

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,39 @@
+function post_message(data){
+  var add_message = {
+    "<li class="chat-message">\
+      <div class="chat-message__header clearfix">\
+        <p class="chat-message__name"> data.name</p>\
+        <p class="chat-message__time"> data.date</p>\
+      </div>\
+      <p class="chat-message__body"> data.message</p>\
+      <p class="chat-message__photo"> data.photo</p>\
+    </li>"
+  };
+  $("#js-post").append(add_message);
+};
+
+$(function() {
+  $('#post-form').on('submit', function(e) {
+    e.preventDefault();
+    var form = $(this);
+    var post_url = form.prop('action');
+    var form_data = from.get(0)
+    var formdata = new Formdata(form_data);
+
+    $.ajax({
+      url: post_url + '.json',
+      type: 'post',
+      dataType: 'json',
+      data: formdata
+      contentType: false,
+      processData: false,
+    });
+      .done(function(data){
+        post_message(data)
+      });
+      .fall(function(){
+        alert("messageを入力してください")
+      });
+  });
+});
+
