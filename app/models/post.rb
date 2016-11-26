269cfc756c5767ee595f56c1ca4e753330fd104d
matsuda tomoki <t.m.0011223344@gmail.com>
2016-11-26 23:20:44 +0900

    Add_post_date

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,12 @@
 class Post < ApplicationRecord
+
   belongs_to :user
   belongs_to :group
   validates :message,presence: true
   mount_uploader :text_image, TextImageUploader
+
+  def post_date
+    created_at.strftime("%Y/ %m/ %d %T")
+  end
+
 end
