987b3831762e9be7eb93b19bf591f43016100252
matsuda tomoki <t.m.0011223344@gmail.com>
2016-11-26 20:47:23 +0900

    Fixed_name(updata)_and_variable

--- app/assets/javascripts/auto_update.js
+++ app/assets/javascripts/auto_update.js
@@ -1,17 +1,17 @@
 $(function() {
-  function post_message(data){
-    for (var i = 0; i < data.messages.length; i++) {
+  function auto_updata(data){
+    for (var i = 0; i < data.length; i++) {
       var add_message ="<li class='chat-message'>\
       <div class='chat-message__header clearfix'>\
-        <p class='chat-message__name'>"+ data.nicknames[i] +"</p>\
-        <p class='chat-message__time'>"+ data.date[i]+"</p>\
+        <p class='chat-message__name'>"+ data[i].nickname +"</p>\
+        <p class='chat-message__time'>"+ data[i].date+"</p>\
       </div>\
-      <p class='chat-message__body'>"+data.messages[i]+"</p>\
+      <p class='chat-message__body'>"+data[i].message+"</p>\
       </li>"
 
-      var image ="<p class='chat-message__photo'><img class='text_image' src = " + data.images[i] +"></p>"
+      var image ="<p class='chat-message__photo'><img class='text_image' src = " + data[i].image +"></p>"
 
-      if (data.images[i] != null){
+      if (data[i].image != null){
         $('ul.chat-messages').append(add_message,image);
       }else{
         $('ul.chat-messages').append(add_message);
@@ -26,7 +26,7 @@ $(function() {
       })
       .done(function(data) {
         $('ul.chat-messages').empty();
-        post_message(data);
+        auto_updata(data);
         $('.chat-body').animate({scrollTop:$('.chat-body')[0].scrollHeight},'fast');
         console.log("更新されたでごわす");
       })
